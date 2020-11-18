99982eb6e9348df1aa1b8b46e9ecb8c64d9c450c
Dominik Káčer <dominik.kacer@ixdata.sk>
2020-11-18 13:10:46 +0100

    vysvetlovanie, quiz updates

---
+++ Assets/scripts/Vysvetlovanie_casovac.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vysvetlovanie_casovac : MonoBehaviour
+{
+    public static float timeLeft = 5f; //cas v sekundach
+    public static bool timerIsRunning = false, cas_uplynul = false;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Odpocet uplynul!");
+                timeLeft = 0;
+                timerIsRunning = false;
+                cas_uplynul = true;
+            }
+        }
+    }
+}
